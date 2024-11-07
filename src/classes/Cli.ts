aaf62fadbd6c9d0764f0ea65b22489ed6ee6d816
Alek Gonzalez <Magicguys1@icloud.com>
2024-11-06 19:12:54 -0700

    copied the car again for motor bike
    copied car again and changed car to motorbike

--- src/classes/Cli.ts
+++ src/classes/Cli.ts
@@ -249,10 +249,15 @@ class Cli {
         },
       ])
       .then((answers) => {
-        // TODO: Use the answers object to pass the required properties to the Motorbike constructor
-        // TODO: push the motorbike to the vehicles array
-        // TODO: set the selectedVehicleVin to the vin of the motorbike
-        // TODO: perform actions on the motorbike
+        const motorbike = new Motorbike
+        Cli.generateVin(),
+        answers.color,
+        answers.make,
+        answers.model
+        parseInt(answers.year),
+        parseInt(answers.weight),
+        parseInt(answers.topSpeed),
+        [],
       });
   }
 
